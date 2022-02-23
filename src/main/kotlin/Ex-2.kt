e728f2d657bbbe950645050068258448df91cc71
João Henrique <joao.aof14@gmail.com>
2022-02-23 15:53:41 -0300

    first ver.

--- src/main/kotlin/Ex-2.kt
+++ src/main/kotlin/Ex-2.kt
@@ -7,17 +7,17 @@ fun main(){
     val num3 = readLine()!!.toDouble()
 
     if(num1<num2 && num2<num3) {
-        print("Ordem crescente: $num1,$num2 e $num3")
+        print("\nOrdem crescente: $num1,$num2 e $num3")
     }else if(num1<num3 && num3<num2) {
-        print("Ordem crescente: $num1,$num3 e $num2")
+        print("\nOrdem crescente: $num1,$num3 e $num2")
     }else if(num2<num1 && num1<num3) {
-        print("Ordem crescente: $num2,$num1 e $num3")
+        print("\nOrdem crescente: $num2,$num1 e $num3")
     } else if(num2<num3 && num3<num1) {
-        print("Ordem crescente: $num2,$num3 e $num1")
+        print("\nOrdem crescente: $num2,$num3 e $num1")
     }else if(num3<num1 && num1<num2) {
-        print("Ordem crescente: $num3,$num1 e $num2")
+        print("\nOrdem crescente: $num3,$num1 e $num2")
     }else{
-        print("Ordem crescente: $num3,$num2 e $num1")
+        print("\nOrdem crescente: $num3,$num2 e $num1")
     }
 
 }
